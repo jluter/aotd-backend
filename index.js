182e68b55dc2a13fedd515541d189937592a2469
Jakob Luter <jakobluter20@gmail.com>
2023-03-14 20:28:08 -0500

    Add axios npm package. Add incomplete authorization flow for spotify API

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ require("dotenv").config()
 const PORT = process.env.PORT || 5050;
 const welcome = `Welcome to ${PORT}`;
 
+const clientId = process.env.client_id;
+const clientSecret = process.env.client_secret;
+
+const basicAuth = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+const authEndpoint = 'https://accounts.spotify.com/api/token';
+
+const authParams = {
+    grant_type: 'client_credentials'
+  };
+
 app.use(express.json());
 app.use(cors());
 
