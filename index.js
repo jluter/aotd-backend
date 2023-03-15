247ce8473be5eeec8abed3c51bb8b6b973908dd6
Jakob Luter <jakobluter20@gmail.com>
2023-03-14 20:41:29 -0500

    Add flow for getting Spotify authorization token.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const fs = require("fs");
+const axios = require('axios');
 
 require("dotenv").config()
 
@@ -11,12 +12,37 @@ const welcome = `Welcome to ${PORT}`;
 const clientId = process.env.client_id;
 const clientSecret = process.env.client_secret;
 
-const basicAuth = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
-const authEndpoint = 'https://accounts.spotify.com/api/token';
+// The authorization endpoint URL for Spotify API
+const authEndpointUrl = 'https://accounts.spotify.com/api/token';
 
-const authParams = {
-    grant_type: 'client_credentials'
-  };
+// Encode the client ID and client secret to Base64
+const authHeader = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+
+// Define the data to send in the request body
+const data = 'grant_type=client_credentials';
+
+// Set the request headers
+const config = {
+  headers: {
+    'Authorization': `Basic ${authHeader}`,
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+};
+
+// Make the POST request to the Spotify API to get an authorization token
+axios.post(authEndpointUrl, data, config)
+  .then(response => {
+    console.log(response.data.access_token);
+  })
+  .catch(error => {
+    console.error(error);
+});
+
+
+
+
+
+
 
 app.use(express.json());
 app.use(cors());
