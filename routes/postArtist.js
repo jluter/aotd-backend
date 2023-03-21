20158e633da2a148c9f60d8eee3cb83438b8463a
Jakob Luter <106687978+jluter@user.noreply.github.com>
2023-03-20 21:59:06 -0500

    Add functionality to handle a POST, take string, write it to artistName.json

--- routes/postArtist.js
+++ routes/postArtist.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const fs = require('fs')
 const app = express();
 const cors = require("cors");
+const e = require('express');
 
 app.use(cors());
 app.use(express.json());
@@ -13,10 +14,14 @@ router.post("/", (req, res) => {
     ) {
       return res.status(400).send("Artist name not entered");
     }
-  
-    console.log(req.body);
 
-    res.send("Posted successfully");
+    const nameObject = {
+        artistName: req.body.artistName
+    };
+
+    fs.writeFileSync('./data/artistName.json', JSON.stringify(nameObject));
+
+    res.status(200).json(nameObject);
   });
 
 module.exports = router
